55ea349097c71a71363eb82f09f3b0cd4319021e
KasiaMiko <74139350+KasiaMiko@users.noreply.github.com>
2020-12-11 19:46:27 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import axios from "axios";
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+  
+        <p>
+          Hello World
+          New message on page
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
